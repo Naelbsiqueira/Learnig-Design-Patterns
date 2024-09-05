742f3c6c15082624688169fafd27b9b4384cb528
Nael Bernardo <naelbsiqueira@hotmail.com>
2024-09-05 09:58:50 -0300

    Inicilization of the project design patterns

---
+++ src/strutural/builder/Person.java
@@ -0,0 +1,8 @@
+package strutural.builder;
+/*Padrão Builder : É um padrão criacional que permite a separação da cronstrução
+de um objeto complexo da sua representação, de forma que o mesmo processo de
+construção possa criar diferentes representações. (construção de objetos com caracterísca
+diferentes)
+Muito utilizado para crição de objetos complexos*/
+public class Person {
+}
